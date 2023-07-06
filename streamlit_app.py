dd275e295dfa265c172e01bb6dcd2a3f7745b316
Margit Hain <71278816+CodeGitte@users.noreply.github.com>
2023-07-06 02:06:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if check_password():
     @st.cache_resource()
     def load_model():
         # Defining the model pipeline from HuggingFace
-        return pipeline(model = "philschmid/instruct-igel-001")
+        return pipeline("philschmid/instruct-igel-001")
     
     # Load the model
     text_generator = load_model()
